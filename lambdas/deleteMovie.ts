70e8a3ddc30a7617afdd77272791da30be79c2c1
Shaobo Zang <20105527@mail.wit.ie>
2024-02-24 02:54:10 +0000

    Exercise: Delete movie endpoint

---
+++ lambdas/deleteMovie.ts
@@ -0,0 +1,60 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  try {
+    console.log("Event: ", event);
+
+    const movieId = event.pathParameters?.movieID;
+    if (!movieId) {
+      return {
+        statusCode: 400,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ message: "Missing movie ID in path parameters" }),
+      };
+    }
+
+    const commandOutput = await ddbDocClient.send(
+      new DeleteCommand({
+        TableName: process.env.TABLE_NAME,
+        Key: { movieID: movieId }, 
+      })
+    );
+
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ message: "Movie deleted successfully" }),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error: `Could not delete movie: ${error.message}` }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
